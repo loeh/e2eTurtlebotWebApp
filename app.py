b2e44c71775aa3cd768159a1aaf8b5d42cf8408f
loeh <loeh@zhaw.ch>
2016-07-27 14:23:28 +0200

    added kubernetes yaml files and added docking REST endpoint to the app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
+
 from flask import Flask, jsonify, json
 from enum import Enum
 import requests, time
@@ -10,19 +11,6 @@ class State(Enum):
 	away = 2
 
 appState = State.home
-
-def getToken():
-	tokenUrl = 'http://ab44513583e1711e6b0ef02e4cf15b02-1319127330.eu-central-1.elb.amazonaws.com:8001/login'
-
-	headers = {'Accept': 'application/json'}
-
-	payload  = {'username':'roboreg',
-                'password':'splab',
-                'eauth':'pam'
-                }
-	r = requests.post(tokenUrl, headers=headers, data=payload)
-
-	return r.headers['X-Auth-Token']
 
 @app.route('/orchestrate/api/v1.0/getState', methods=['GET'])
 def getState():
@@ -38,27 +26,47 @@ def run():
         return 'went away'
     elif appState == State.away:
         goHome()
-        docking()
         appState = State.home
-        return 'got home and docked'
+        return 'got home'
+
+@app.route('/orchestrate/api/v1.0/dock', methods=['POST'])
+def docking():
+    # kill running nodes
+    # start docking nodes 
+    invoceCommandOnRobot('rosnode kill -a')
+    invoceCommandOnRobot('/home/turtlebot/launch_docking.sh')
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+def getToken():
+    tokenUrl = 'http://ab44513583e1711e6b0ef02e4cf15b02-1319127330.eu-central-1.elb.amazonaws.com:8001/login'
+
+    headers = {'Accept': 'application/json'}
+
+    payload  = {'username':'roboreg',
+        'password':'splab',
+        'eauth':'pam'
+                }
+    r = requests.post(tokenUrl, headers=headers, data=payload)
+
+    return r.headers['X-Auth-Token']
 
 def goAway():
-    startNodeOnRobot('/home/turtlebot/launch_minimal.sh')
+    invoceCommandOnRobot('/home/turtlebot/launch_minimal.sh')
     time.sleep(5)
-    startNodeOnRobot('/home/turtlebot/launch_amcl.sh')
+    invoceCommandOnRobot('/home/turtlebot/launch_amcl.sh')
     time.sleep(5)
-    startNodeOnRobot('/home/turtlebot/launch_moveto_away.sh')
+    invoceCommandOnRobot('/home/turtlebot/launch_moveto_away.sh')
     # publish to a move_base topic
 
 def goHome():
     startNodeOnRobot('/home/turtlebot/launch_moveto_home.sh')
+    
 
-def docking():
-    # kill running nodes
-    # start docking nodes 
-    print 'docking now'
-
-def startNodeOnRobot(command):
+def invoceCommandOnRobot(command):
     url = 'http://ab44513583e1711e6b0ef02e4cf15b02-1319127330.eu-central-1.elb.amazonaws.com:8001'
 
     headers = {'Accept': 'application/json',
@@ -74,10 +82,6 @@ def startNodeOnRobot(command):
     r = requests.post(url, headers=headers, data=payload)
 
     print 'started node'
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
